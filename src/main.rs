c7680b24be9ce53457945ae6f741fa7ef9da9eee
Anisse Astier <anisse@astier.eu>
2023-09-24 20:53:46 +0200

    use anyhow instead of ad-hoc string errors

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ use std::env;
 use std::io;
 use std::io::Write;
 
+use anyhow::{bail, Context, Result};
 use serde::Deserialize;
 
 const USER_AGENT: &str = concat!("hashtag-importer v", env!("CARGO_PKG_VERSION"));
+const CLIENT_NAME: &str = "hashtag-importer test version";
+const CLIENT_WEBSITE: &str = "https://github.com/anisse/hashtag-importer?soon";
+
 fn main() {
     // We'll add an argument parser later
     let args: Vec<_> = env::args().collect();
@@ -19,40 +23,39 @@ fn main() {
         _ => panic!("Unsupported action"),
     }
 }
-fn client() -> Result<reqwest::blocking::Client, String> {
+fn client() -> Result<reqwest::blocking::Client> {
     reqwest::blocking::Client::builder()
         .timeout(std::time::Duration::from_secs(20))
         .user_agent(USER_AGENT)
         .cookie_store(true)
         .build()
-        .map_err(|e| format!("Cannot build custom client: {e}"))
+        .with_context(|| "cannot build custom client")
 }
 
-
-fn create_app() -> Result<(), String> {
+fn create_app() -> Result<()> {
     print!("Enter your mastodon server api domain name: ");
-    io::stdout().flush().map_err(|e| format!("flush: {e}"))?;
+    io::stdout().flush().with_context(|| "flush")?;
     let mut server_domain = String::new();
     io::stdin()
         .read_line(&mut server_domain)
-        .map_err(|e| format!("unable to read stdin: {e}"))?;
+        .with_context(|| "unable to read stdin")?;
     let url = reqwest::Url::parse(format!("https://{server_domain}/").as_str())
-        .map_err(|e| format!("{server_domain} is not a domain: {e}"))?;
+        .with_context(|| format!("{server_domain} is not a domain"))?;
     // Register the app
     // TODO: struct + deserialize instead
     let resp: HashMap<String, String> = client()?
-        .post(url.join("api/v1/apps").map_err(|e| format!("{e}"))?)
+        .post(url.join("api/v1/apps")?)
         .json(&HashMap::from([
             // TODO: struct + serialize instead
-            ("client_name", "hashtag-importer test version"),
+            ("client_name", CLIENT_NAME),
             ("redirect_uris", "urn:ietf:wg:oauth:2.0:oob"),
-            ("website", "https://github.com/anisse/hashtag-importer?soon"),
+            ("website", CLIENT_WEBSITE),
             ("scopes", "read"),
         ]))
         .send()
-        .map_err(|e| format!("post failed: {e}"))?
+        .with_context(|| "create app post failed")?
         .json()
-        .map_err(|e| format!("body not valid json: {e}"))?;
+        .with_context(|| "create app body not valid json")?;
     println!("Copy paste this into your config.toml:");
     println!("[auth]");
     println!("client_id = '{}'", resp["client_id"]);
@@ -72,22 +75,21 @@ struct Auth {
     token: String,
 }
 
-fn user_auth() -> Result<(), String> {
+fn user_auth() -> Result<()> {
     let config: Config = toml::from_str(
-        &std::fs::read_to_string("config.toml")
-            .map_err(|e| format!("cannot read config.toml: {e}"))?,
+        &std::fs::read_to_string("config.toml").with_context(|| "cannot read config.toml")?,
     )
-    .map_err(|e| format!("invalid config: {e}"))?;
+    .with_context(|| "invalid config")?;
     webbrowser::open(&format!(
         "https://{}/oauth/authorize?response_type=code&client_id={}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=read",
         config.server, config.auth.client_id,
     ))
-    .map_err(|e| format!("cannot open browser: {e}"))?;
+    .with_context(|| "cannot show auth in browser")?;
     println!("Paste the code your server gave you:");
     let mut code = String::new();
     io::stdin()
         .read_line(&mut code)
-        .map_err(|e| format!("unable to read stdin: {e}"))?;
+        .with_context(|| "unable to read stdin")?;
     let token = token(
         &config.server,
         &config.auth.client_id,
@@ -100,7 +102,7 @@ fn user_auth() -> Result<(), String> {
     Ok(())
 }
 
-fn run() -> Result<(), String> {
+fn run() -> Result<()> {
     /*
     let body = reqwest::blocking::get("https://mastodon.social/api/v1/timelines/tag/fakeshakespearefacts?max_id=111111639034423756")
         .map_err(|e| format!("get failed: {e}"))?
@@ -109,10 +111,9 @@ fn run() -> Result<(), String> {
     println!("{body}");
     */
     let config: Config = toml::from_str(
-        &std::fs::read_to_string("config.toml")
-            .map_err(|e| format!("cannot read config.toml: {e}"))?,
+        &std::fs::read_to_string("config.toml").with_context(|| "cannot read config.toml")?,
     )
-    .map_err(|e| format!("invalid config: {e}"))?;
+    .with_context(|| "invalid config")?;
     hashtags(&config.server, &config.auth.token)
 }
 
@@ -126,12 +127,7 @@ struct Token {
     scope: String,
     */
 }
-fn token<S: AsRef<str>>(
-    server: S,
-    client_id: S,
-    client_secret: S,
-    code: S,
-) -> Result<String, String> {
+fn token<S: AsRef<str>>(server: S, client_id: S, client_secret: S, code: S) -> Result<String> {
     let response = client()?
         .post(format!("https://{}/oauth/token", server.as_ref()))
         .json(&HashMap::from([
@@ -144,23 +140,21 @@ fn token<S: AsRef<str>>(
             ("scope", "read"),
         ]))
         .send()
-        .map_err(|e| format!("post failed: {e}"))?;
+        .with_context(|| "token post failed")?;
     let status_err = response.error_for_status_ref();
     if let Err(e) = status_err {
-        return Err(format!(
+        bail!(
             "Got response {}: {e}",
-            response
-                .text()
-                .map_err(|e| format!("body not valid text: {e}"))?
-        ));
+            response.text().with_context(|| "body not valid text")?
+        );
     }
     let token: Token = response
         .json()
-        .map_err(|e| format!("token body not valid json: {e}"))?;
+        .with_context(|| "token body not valid json")?;
     Ok(token.access_token)
 }
 
-fn hashtags(server: &str, token: &str) -> Result<(), String> {
+fn hashtags(server: &str, token: &str) -> Result<()> {
     let response = client()?
         .get(format!(
             //"https://{server}/api/v2/search?q=https%3A%2F%2Fmastodon.social%2F%40jfstudiospaleoart%40sauropods.win%2F111112209821147036&resolve=true&limit=11&type=statuses"
@@ -168,7 +162,7 @@ fn hashtags(server: &str, token: &str) -> Result<(), String> {
         ))
         .header("Authorization", format!("Bearer {token}"))
         .send()
-        .map_err(|e| format!("get failed: {e}"))?
+        .with_context(|| "hashtags get failed")?
         .text();
     dbg!(response.unwrap());
     Ok(())
