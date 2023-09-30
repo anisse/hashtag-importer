c1891bff2668640c6554db78fea5357cfd7a0aa9
Anisse Astier <anisse@astier.eu>
2023-09-30 17:57:06 +0200

    Move to clap for argument parsing
    Add description and license to Cargo.toml for clap usage.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,35 @@ mod config;
 mod hashtag_importer;
 mod types;
 
-use std::env;
+use std::error::Error;
 
-fn main() {
-    // We'll add an argument parser later
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 2 {
-        println!("Usage: {} run|associate", args[0])
+use clap::{Parser, Subcommand};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+    match &cli.command {
+        // TODO: merge both first steps + toml-edit for init ?
+        Commands::CreateApp => hashtag_importer::create_app()?,
+        Commands::UserAuth => hashtag_importer::user_auth()?,
+        Commands::Run => hashtag_importer::run()?,
     }
-    match args[1].as_str() {
-        "create-app" => hashtag_importer::create_app().unwrap(),
-        "user-auth" => hashtag_importer::user_auth().unwrap(),
-        "run" => hashtag_importer::run().unwrap(),
-        _ => panic!("Unsupported action"),
-    }
+    Ok(())
+}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create app when run for the first time; registers app on your server
+    CreateApp,
+    /// Authenticate user on server described on config.toml in order to get a token; needs the app
+    /// to have been created already.
+    UserAuth,
+    /// Run actual service based on config.toml
+    Run,
 }
